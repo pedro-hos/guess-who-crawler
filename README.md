eb5359c3be1cc547dd495ce8aa2a42b3c4a84a81
Pedro Hos <pedro-hos@outlook.com>
2024-07-09 13:20:41 -0300

    scrapping people

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # guess-who-web
 
 1. Salvar o link dos estados num primeiro processo
-
-
-
-5.104678557s
--- controllers/Scraper.go
+++ controllers/Scraper.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"github.com/pedro-hos/guess-who-web/database"
 	"github.com/pedro-hos/guess-who-web/models"
@@ -26,10 +27,7 @@ func federatedUnitBrazilScrap() {
 	c := colly.NewCollector()
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
-	})
-
+	c.OnRequest(func(r *colly.Request) {})
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Panic("Something went wrong: ", err)
 	})
@@ -37,7 +35,7 @@ func federatedUnitBrazilScrap() {
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		if isStateCategoryLink(link) {
-			uf := clearStateName(e.Text)
+			uf := clearCityAndStateNames(e.Text)
 
 			state := models.State{}
 			database.DB.Where(&models.State{Name: uf}).First(&state)
@@ -51,7 +49,6 @@ func federatedUnitBrazilScrap() {
 	})
 
 	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(r.Request.URL, " scraped!")
 		citiesScrap(ufLinks)
 	})
 
@@ -70,7 +67,8 @@ func citiesScrap(states map[*models.State]string) {
 			continue
 		}
 
-		fmt.Println(uf.Name)
+		fmt.Println("#### Scrappring State: " + uf.Name + " ####")
+
 		c := colly.NewCollector()
 		c.OnRequest(func(r *colly.Request) {})
 
@@ -82,23 +80,40 @@ func citiesScrap(states map[*models.State]string) {
 
 			e.ForEach(".mw-category-group", func(_ int, elem *colly.HTMLElement) {
 				h3Title := elem.ChildText("h3") //From A to Z, however, some pages have ~ and empty strings that we don't care;
+				elem.ForEach("a[href]", func(_ int, elem2 *colly.HTMLElement) {
+					cityName := clearCityAndStateNames(elem2.Text)
 
-				if regexp.MustCompile(`[A-Z]`).MatchString(h3Title) {
-					elem.ForEach("a[href]", func(_ int, elem2 *colly.HTMLElement) {
-						city := elem2.Text
+					//This is because the São Paulo and Rio de Janeiro has cities with the same name
+					isSpOrRJCityNatural := cityName == "Naturais da cidade de São Paulo" || cityName == "Naturais da cidade do Rio de Janeiro"
+
+					if regexp.MustCompile(`[A-Z]`).MatchString(h3Title) || isSpOrRJCityNatural {
 						link := elem2.Attr("href")
-						fmt.Println(city + " >>> " + link)
-					})
-				} else {
-					fmt.Println("Empty or ~")
+
+						fmt.Println(cityName + " >>> " + link)
+
+						e.Request.Visit(wikipediaUrl + link)
+					}
+				})
+
+			})
+
+			alredyVisited := false
+			e.DOM.Before(".mw-content-ltr").Find("a[href]").Each(func(i int, s *goquery.Selection) {
+				link, _ := s.Attr("href")
+
+				if !alredyVisited && s.Text() == "página seguinte" {
+					alredyVisited = true
+					e.Request.Visit(wikipediaUrl + link)
 				}
 			})
+
 		})
 
-		c.OnScraped(func(r *colly.Response) {
-			//fmt.Println(r.Request.URL, " scraped!")
+		c.OnHTML("#mw-pages", func(e *colly.HTMLElement) {
+			//fmt.Println("Page " + e.Text)
 		})
 
+		c.OnScraped(func(r *colly.Response) {})
 		c.Visit(link)
 
 	}
@@ -108,7 +123,7 @@ func citiesScrap(states map[*models.State]string) {
 	fmt.Println(diff)
 }
 
-func clearStateName(text string) string {
+func clearCityAndStateNames(text string) string {
 	uf := strings.ReplaceAll(text, "Naturais do estado de ", "")
 	uf = strings.ReplaceAll(uf, "Naturais do estado do ", "")
 	uf = strings.ReplaceAll(uf, "Naturais do ", "")
